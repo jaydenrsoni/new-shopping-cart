e2968c069df7d743bd3951db02fed78f20b43336
Jayden Soni <jrs@u.northwestern.edu>
2019-04-16 14:45:12 -0500

    Initial deploy

---
+++ build/precache-manifest.ee79ac7a0a19034cadb7950157b2f2d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a202cec773ac4d3fa2fd",
+    "url": "/static/css/main.f1a8adee.chunk.css"
+  },
+  {
+    "revision": "a202cec773ac4d3fa2fd",
+    "url": "/static/js/main.5ddd1724.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "350d3074b5ab6c1bfa0482de1e30ee92",
+    "url": "/index.html"
+  }
+];
